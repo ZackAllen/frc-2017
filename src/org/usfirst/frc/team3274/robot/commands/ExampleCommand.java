1e010fd0c6b7a49412c17b66f1486d2db414ffab
Dell-Laptop-FLL <Dell-Laptop-FLL@134.126.173.105>
2017-01-31 19:18:05 -0500

    Updated Comments

--- src/org/usfirst/frc/team3274/robot/commands/ExampleCommand.java
+++ src/org/usfirst/frc/team3274/robot/commands/ExampleCommand.java
@@ -8,35 +8,50 @@ import org.usfirst.frc.team3274.robot.Robot;
  *
  */
 public class ExampleCommand extends Command {
-	public ExampleCommand() {
+/* nathan make some commands */
+    public ExampleCommand() {
 		// Use requires() here to declare subsystem dependencies
 		//requires(Robot.exampleSubsystem);
 	}
 
-	// Called just before this Command runs the first time
-	@Override
-	protected void initialize() {
-	}
+    /** 
+     * Called just before this Command runs the first time.
+     */
+    @Override
+    protected void initialize() {
+    }
 
-	// Called repeatedly when this Command is scheduled to run
-	@Override
-	protected void execute() {
-	}
+    /**
+     * Called repeatedly when this Command is scheduled to run.
+     */
+    @Override
+    protected void execute() {
+    }
 
-	// Make this return true when this Command no longer needs to run execute()
-	@Override
-	protected boolean isFinished() {
-		return false;
-	}
+    /**
+     * Make this return true when this Command no longer needs to run execute().
+     */
+    @Override
+    protected boolean isFinished() {
+        return false;
+    }
 
-	// Called once after isFinished returns true
-	@Override
-	protected void end() {
-	}
+    /**
+     * Called once after isFinished returns true.
+     */
+    @Override
+    protected void end() {
+    }
 
-	// Called when another command which requires one or more of the same
-	// subsystems is scheduled to run
-	@Override
-	protected void interrupted() {
-	}
+    /**
+     * Called when another command which requires one or more of the
+     * same(non-Javadoc)
+     * 
+     * {@see edu.wpi.first.wpilibj.command.Command#interrupted()}
+     * 
+     * subsystems is scheduled to run.
+     */
+    @Override
+    protected void interrupted() {
+    }
 }
