94e4c19ca7e24009108f36ce92f0fa26983e7c65
Dell-Laptop-FLL <Dell-Laptop-FLL@134.126.173.105>
2017-01-31 20:04:10 -0500

    Fixed Bugs
    Fixed NullPointerException

--- src/org/usfirst/frc/team3274/robot/Robot.java
+++ src/org/usfirst/frc/team3274/robot/Robot.java
@@ -7,13 +7,11 @@ import edu.wpi.first.wpilibj.livewindow.LiveWindow;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
-import org.usfirst.frc.team3274.robot.commands.DriveAndShootAutonomous;
 import org.usfirst.frc.team3274.robot.commands.DriveForward;
-import org.usfirst.frc.team3274.robot.subsystems.Collector;
+import org.usfirst.frc.team3274.robot.commands.DriveWithJoystick;
+import org.usfirst.frc.team3274.robot.commands.RunAutonomous;
+import org.usfirst.frc.team3274.robot.commands.RunTeleop;
 import org.usfirst.frc.team3274.robot.subsystems.DriveTrain;
-import org.usfirst.frc.team3274.robot.subsystems.Pivot;
-import org.usfirst.frc.team3274.robot.subsystems.Pneumatics;
-import org.usfirst.frc.team3274.robot.subsystems.Shooter;
 
 /**
  * This is the main class for running the PacGoat code.
@@ -24,16 +22,43 @@ import org.usfirst.frc.team3274.robot.subsystems.Shooter;
  * creating this project, you must also update the manifest file in the resource
  * directory.
  */
+/* Nathan ....... this is a robot */
+
+/*
+ * ----- Some notes on GIT ----- git makes a copy of the whole filesystem
+ * whenever you clone a repo. This means you 'checkout' everything. Once you
+ * have done some work and you are ready to save your changes to git, use the
+ * 'commit' command. This saves the changeset to your local git repo. The
+ * 'staging' file will be updated with the list of changes that you have made.
+ * At this step you must use the 'pull' command to see if others may have pushed
+ * changes to the code to the server. If there are conflicts between the files
+ * on the server and the files on your computer you must 'merge' them. To tell
+ * the git server about your changes so that other users and computers can
+ * access them, use the 'push' command. This takes any changes stored in the
+ * staging file and tells the server about them. More info is available at
+ * https://git-scm.com/book/en/v2/Getting-Started-About-Version-Control
+ */
 public class Robot extends IterativeRobot {
     Command autonomousCommand;
+    Command teleopCommand;
+    /* our joystick */
     public static OI oi;
-    public static boolean isTeleop;
+    /* flag for teleop mode */
+    /*
+     * 'public static' means that this variable can be accessed on the class
+     * without creating an instance. IE this can be accessed by 'Robot.isTeLeop'
+     * instead of having to do: Robot r = new Robot(); r.isTeLeop;
+     * 
+     * More info at
+     * https://www.caveofprogramming.com/java/java-for-beginners-static-
+     * variables-what-are-they.html
+     */
 
     // Initialize the subsystems
     public static DriveTrain drivetrain = new DriveTrain();
 
+    // ignore the "raw type" warning
     public SendableChooser autoChooser;
-    public SendableChooser autonomousDirectionChooser;
 
     // This function is run when the robot is first started up and should be
     // used for any initialization code.
@@ -48,20 +73,22 @@ public class Robot extends IterativeRobot {
         // news. Don't move it.
         oi = new OI();
 
-        isTeleop = false;
-
-// // instantiate the command used for the autonomous period
-// autoChooser = new SendableChooser();
-// autoChooser.addDefault("Drive and Shoot", new DriveAndShootAutonomous());
-// autoChooser.addObject("Drive Forward", new DriveForward());
-// SmartDashboard.putData("Auto Mode", autoChooser);
+        // instantiate the command used for the autonomous period
+        autoChooser = new SendableChooser();
+        autoChooser.addDefault("Autonomous Control", new RunAutonomous());
+        autoChooser.addObject("Drive Forward", new DriveForward());
+        SmartDashboard.putData("Auto Mode", autoChooser);
     }
 
+    /*
+     * Code that runs when autonomous mode is started
+     * 
+     * @see edu.wpi.first.wpilibj.IterativeRobot#autonomousInit()
+     */
     @Override
     public void autonomousInit() {
         autonomousCommand = (Command) autoChooser.getSelected();
         autonomousCommand.start();
-        isTeleop = false;
     }
 
     // This function is called periodically during autonomous
@@ -71,6 +98,11 @@ public class Robot extends IterativeRobot {
         log();
     }
 
+    /*
+     * Code that runs when tele op mode is started
+     * 
+     * @see edu.wpi.first.wpilibj.IterativeRobot#teleopInit()
+     */
     @Override
     public void teleopInit() {
         // This makes sure that the autonomous stops running when
@@ -80,16 +112,20 @@ public class Robot extends IterativeRobot {
         if (autonomousCommand != null) {
             autonomousCommand.cancel();
         }
-        isTeleop = true;
 
         // instantiate commands for teleop driving
-        autoChooser.addDefault("Drive and Shoot",
-                new DriveAndShootAutonomous());
+        autoChooser.addDefault("Teleop Control", new RunTeleop());
     }
 
     // This function is called periodically during operator control
     @Override
     public void teleopPeriodic() {
+
+        /*
+         * Note that the DriveWithJoyStick command is activated automatically in
+         * DriveTrain.initDefaultCommand()
+         */
+
         Scheduler.getInstance().run();
         log();
     }
